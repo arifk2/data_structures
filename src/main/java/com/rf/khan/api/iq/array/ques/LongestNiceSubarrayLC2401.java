2f43ee41ecd14c07bd7200d56ae1fff06750f4f6
Mohd Arif Khan <moharifk10@gmail.com>
2022-09-06 02:24:16 +0530

    Longest Nice Subarray LC2401

---
+++ src/main/java/com/rf/khan/api/iq/array/ques/LongestNiceSubarrayLC2401.java
@@ -0,0 +1,54 @@
+package com.rf.khan.api.iq.array.ques;
+
+/**
+ * 
+ * @author mkhan339
+ *
+ */
+public class LongestNiceSubarrayLC2401 {
+
+	/**
+	 * This method will return the length of the longest array
+	 * 
+	 * @param nums
+	 * @return
+	 */
+	public int longestNiceSubarray(int[] nums) {
+
+		int ans = Integer.MIN_VALUE;
+		int startIndex = 0;
+
+		int bitsUnion = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			int newElement = nums[i];
+
+			// violate the window
+			while ((bitsUnion & newElement) != 0) {
+				int startingElement = nums[startIndex];
+
+				// Remove element from start
+				bitsUnion ^= startingElement;
+				startIndex++;
+			}
+
+			// happy case
+			// Including new element
+			bitsUnion = bitsUnion | newElement;
+			ans = Math.max(ans, i - startIndex + 1);
+		}
+
+		return ans;
+	}
+
+	/**
+	 * Driver main method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LongestNiceSubarrayLC2401 l = new LongestNiceSubarrayLC2401();
+		int arr[] = { 1, 3, 8, 48, 10 };
+		System.out.println(l.longestNiceSubarray(arr));
+	}
+}
