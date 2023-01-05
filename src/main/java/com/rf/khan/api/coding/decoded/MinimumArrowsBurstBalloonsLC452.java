eed19ee8aa42afafea3489c8d3b2b63b4f336712
Mohd Arif Khan <moharifk10@gmail.com>
2023-01-05 23:19:37 +0530

    452. Minimum Number of Arrows to Burst Balloons

---
+++ src/main/java/com/rf/khan/api/coding/decoded/MinimumArrowsBurstBalloonsLC452.java
@@ -0,0 +1,26 @@
+package com.rf.khan.api.coding.decoded;
+
+import java.util.Arrays;
+
+public class MinimumArrowsBurstBalloonsLC452 {
+
+	public static void main(String[] args) {
+		MinimumArrowsBurstBalloonsLC452 m = new MinimumArrowsBurstBalloonsLC452();
+		System.out.println(m.findMinArrowShots(new int[][] { { 10, 16 }, { 2, 8 }, { 1, 6 }, { 7, 12 } }));
+	}
+
+	public int findMinArrowShots(int[][] points) {
+		Arrays.sort(points, (a, b) -> a[1] - b[1]);
+
+		int arrow = 1;
+		int reach = points[0][1];
+
+		for (int i = 1; i < points.length; i++) {
+			if (points[i][0] > reach) {
+				arrow++;
+				reach = points[i][1];
+			}
+		}
+		return arrow;
+	}
+}
