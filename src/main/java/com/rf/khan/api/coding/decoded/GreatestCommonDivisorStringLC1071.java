dda869d310b30a6f7b6768a475ee6177922760b2
Mohd Arif Khan <moharifk10@gmail.com>
2023-02-02 10:26:04 +0530

    Leetcode 1071. Greatest Common Divisor of Strings

---
+++ src/main/java/com/rf/khan/api/coding/decoded/GreatestCommonDivisorStringLC1071.java
@@ -0,0 +1,35 @@
+package com.rf.khan.api.coding.decoded;
+
+public class GreatestCommonDivisorStringLC1071 {
+
+	public static void main(String[] args) {
+		GreatestCommonDivisorStringLC1071 g = new GreatestCommonDivisorStringLC1071();
+		System.out.println(g.gcdOfStrings("ABCABC", "ABC"));
+		System.out.println(g.gcdOfStrings("LEET", "CODE"));
+	}
+
+	public String gcdOfStrings(String str1, String str2) {
+		if (!(str1 + str2).equals(str2 + str1))
+			return "";
+
+		// get the GCD of the two lengths
+		int gcdLength = gcd(str1.length(), str2.length());
+
+		return str1.substring(0, gcdLength);
+	}
+
+	/**
+	 * GCD by euclid theorem
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public int gcd(int x, int y) {
+		if (y == 0) {
+			return x;
+		} else {
+			return gcd(y, x % y);
+		}
+	}
+}
